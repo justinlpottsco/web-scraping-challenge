1d0cb4a8657ea0d667abb97785e1a375a9ca6de4
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2020-12-11 09:36:28 -0700

    resave marsscrapepy

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 from splinter import Browser
 from bs4 import BeautifulSoup 
 import time
 import pandas as pd
 from pprint import pprint
 from webdriver_manager.chrome import ChromeDriverManager
-import PyMongo
 
 
-# In[2]:
+# In[3]:
 
 
 # Setup splinter (day2ex7)
@@ -15,7 +20,7 @@ executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-# In[3]:
+# In[4]:
 
 
 # Scrape NASA site
@@ -23,7 +28,7 @@ url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2C
 browser.visit(url)
 
 
-# In[4]:
+# In[5]:
 
 
 # Parse HTML with Beautiful Soup    
@@ -31,7 +36,7 @@ html = browser.html
 soup = BeautifulSoup(html, 'html.parser')
 
 
-# In[5]:
+# In[6]:
 
 
 # Retrieve latest news title
@@ -40,7 +45,7 @@ news_title = most_recent_article.find('h3').get_text()
 print(news_title)
 
 
-# In[6]:
+# In[7]:
 
 
 # Retrieve latest paragraph text
@@ -48,7 +53,7 @@ news_p = most_recent_article.find('div', class_ = 'rollover_description_inner').
 print(news_p)
 
 
-# In[73]:
+# In[8]:
 
 
 # Visit the url for JPL Featured Space Image
@@ -56,42 +61,38 @@ featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mar
 browser.visit(featured_image_url)
 
 
-# In[74]:
+# In[9]:
 
 
 # Find full size image url for current Featured Mars Image
-browser.click_link_by_partial_text('full_image')
+full_image = browser.find_by_id('full_image')
+full_image.click()
 
 html = browser.html
 soup = BeautifulSoup(html, 'html.parser')
 
-main_url = 'https://www.jpl.nasa.gov'
-image_url = soup.find('a', class_ = 'button fancybox').find('img')['src']
+main_url = 'https://www.jpl.nasa.gov/'
+image_url = soup.find('a', class_ = 'button fancybox')['data-fancybox-href']
 
 featured_image_url =  main_url + image_url
 
 print(featured_image_url)
 
 
-# In[17]:
-
-
-# Example:
-featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA16225_hires.jpg'
-
-
-# In[18]:
+# In[10]:
 
 
 # Visit Mars facts url 
 facts_url = 'https://space-facts.com/mars/'
 
-fact_table = pd.read_html(facts_url)
-fact_table
+fact_table = pd.read_html(facts_url)[0]
+fact_table = fact_table.set_index(0)
+fact_table.rename(columns = {
+    1: ""
+}).rename_axis(index = {0:""})
 
 
-
-# In[78]:
+# In[11]:
 
 
 #Visit the USGS Astrogeology site here to obtain high resolution images for each of Mar's hemispheres.
@@ -99,7 +100,7 @@ hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enh
 browser.visit(hemispheres_url)
 
 
-# In[79]:
+# In[12]:
 
 
 # Parse HTML with Beautiful Soup    
@@ -107,7 +108,7 @@ html = browser.html
 soup = BeautifulSoup(html, 'html.parser')
 
 
-# In[80]:
+# In[13]:
 
 
 #You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
@@ -132,13 +133,10 @@ for hemi in hemispheres:
     imgs_url = img_soup.find('img', class_ = 'wide-image')['src']
     
     image_url = mainimg_url + imgs_url
-    hemispheres_images_urls.append({'title': title, 'img_url': image_url})
+    hemispheres_image_urls.append({'title': title, 'img_url': image_url})
 
-print(hemispheres_images_urls)
-
-
+print(hemispheres_image_urls)
 
 
-
-
+# In[ ]:
 
